3d64d4aaf0816ec231b073ee9b1633c49b7c2465
Игорь (JS) <igor.js@hitech18.online>
2018-08-06 18:29:30 +0300

    Clear code.

--- dev.js
+++
@@ -1,7 +0,0 @@
-const ConsoleParser = require('./libs/ConsoleParser');
-
-void async function doit() {
-	let contaners = await  ConsoleParser.getDockerInfo();
-	// TODO: clear
-	console.log('contaners ', contaners);
-}();
